9c71afc708af4195aead7c336a1cd058a80f9f22
cymback <caspergrozz@gmail.com>
2018-09-27 16:05:21 +0200

    work started on menu, all day sketch

---
+++ app/src/main/java/com/example/sulivet/sulivet/Activities/MenuTestActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sulivet.sulivet.Activities
+
+import android.annotation.SuppressLint
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.CardView
+import android.view.View
+import android.widget.ImageView
+import com.example.sulivet.sulivet.R
+
+class MenuTestActivity : AppCompatActivity() {
+
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_test)
+
+    }
+}
